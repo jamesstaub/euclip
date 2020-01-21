5490aaba258fe0cdaf5ba3306f069bb264964b28
James Staub <james@mapdwell.com>
2020-01-21 15:23:20 -0500

    adds dropdown menu component

--- app/components/track-container.js
+++ app/components/track-container.js
@@ -3,6 +3,11 @@ import { action } from '@ember/object';
 export default class TrackContainerComponent extends Component {
   
   @action
+  deleteTrack() {
+    this.args.track.destroyRecord();
+  }
+
+  @action
   updateTrackSequence(value, sequencer) {
     //if sequence is different than euclidean output 
     this.args.track.set('customSequence', sequencer.matrix.pattern[0]);
