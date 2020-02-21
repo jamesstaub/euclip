72b53a092f00b66b880092e48f205bf4996a8391
James Staub <admin@Jamess-MacBook-Pro.local>
2020-02-21 09:45:07 -0500

    home rolled sequencer much simpler!

---
+++ app/components/sequencer-step.js
@@ -0,0 +1,14 @@
+import Component from '@glimmer/component';
+
+export default class SequencerStepComponent extends Component {
+  get fill() {
+    const {idx, stepIndex, step} = this.args;
+    if (idx === stepIndex) {
+      return '#d9534f'
+    } else if (step) {
+      return '#52ebff'
+    } else {
+      return '#fff'
+    }
+  }
+}
--- app/components/track-sequencer.js
+++
@@ -1,5 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-
-export default class TrackSequencerComponent extends Component {
-}
