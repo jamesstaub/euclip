c4fd369182b2f094bb0fce2eb48d3e4dafb95273
jamesstaub <james.staub@gmail.com>
2020-04-21 23:38:40 -0400

    delete track route handling

--- app/components/track-container.js
+++ app/components/track-container.js
@@ -7,9 +7,7 @@ export default class TrackContainerComponent extends Component {
 
   @action
   async deleteTrack() {
-    const projectTracks = this.args.track.get('project.tracks');
-    await this.args.track.destroyAndCleanup();
-    this.router.transitionTo('user.creator.project.track', projectTracks.firstObject);
+    this.args.track.destroyAndCleanup();
   }
 
   @action
