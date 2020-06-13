6869722ac162c7a8387baccce19ec6910fd66d84
jamesstaub <james.staub@gmail.com>
2020-06-13 17:48:51 -0400

    style fix for tabs

--- app/components/tab-menu.js
+++ app/components/tab-menu.js
@@ -1,9 +1,17 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { isPresent } from '@ember/utils';
 
 export default class TabMenuComponent extends Component {
   @tracked selectedValue
+
+  get val() {
+    if (isPresent(this.selectedValue)) {
+      return this.selectedValue;
+    }
+    return this.args.selectedValue;
+  }
 
   @action
   select(value) {
