5a386ec3fd2e6ce3a864fa35e3f611fe248f9d90
jamesstaub <james.staub@gmail.com>
2020-06-14 13:38:01 -0400

    dropdown menu with click events and position

--- app/components/dropdown-menu.js
+++ app/components/dropdown-menu.js
@@ -4,9 +4,27 @@ import { tracked } from '@glimmer/tracking';
 
 export default class DropdownMenuComponent extends Component {
   @tracked open = false;
+  @tracked menuX;
+  @tracked menuY;
+
+  offsetX = -80;
+  offsetY = 35;
   
   @action
-  toggleOpen() {
+  addPositionClass(list) {
+    list.style.top = `${this.menuY}px`;
+    list.style.left = `${this.menuX}px`;
+  }
+
+  @action
+  setPosition(btn) {
+    const {x, y} = btn.getClientRects()[0];
+    this.menuX = x + this.offsetX;
+    this.menuY = y + this.offsetY;
+  }
+
+  @action
+  toggleOpen() {   
     this.open = !this.open;
   }
 }
